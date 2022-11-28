9557e18d4094e19e302cec15a0b395b950527c5f
Enzo Sanchez Croce <enzo.sanchez@iquall.net>
2022-11-28 19:34:17 -0300

    Fix imagenes de corriente y marco teorico

--- scripts/csv_to_image.py
+++ scripts/csv_to_image.py
@@ -30,9 +30,9 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 fig,ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 4))
 ax.xaxis.set_major_formatter(EngFormatter(unit='s'))
-ax.yaxis.set_major_formatter(EngFormatter(unit='V'))
+ax.yaxis.set_major_formatter(EngFormatter(unit='A'))
 ax.set_xlabel('Tiempo')
-ax.set_ylabel('Tensión')
+ax.set_ylabel('Corriente')
 #enable grid
 ax.grid(True)
 #ax.set_title(sys.argv[3])
@@ -40,7 +40,7 @@ ax.grid(True)
 if len(sys.argv) == 4:
     ax.set_xlim(float(sys.argv[2])-260e-9, float(sys.argv[3])-260e-9)
 # Divide y values by 10.8
-#y = [i / 10.8 for i in y]
+y = [i * 0.2 for i in y]
 # Inverti y values
 #y = [-i for i in y]
 ax.plot(x,y,color='red', linewidth=0.7)
