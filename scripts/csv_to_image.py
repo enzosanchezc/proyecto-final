b67cf93cd657a305d6ca59faf55a63d3eedb4faf
Enzo Sanchez Croce <enzo.sanchez@iquall.net>
2022-12-04 22:16:47 -0300

    Ver descripción del commit
    -Terminar de comparar imagenes en el tex de simulaciones prototipo
    -Agregar teoria del selector de modo

--- scripts/csv_to_image.py
+++ scripts/csv_to_image.py
@@ -30,9 +30,9 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 fig,ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 4))
 ax.xaxis.set_major_formatter(EngFormatter(unit='s'))
-ax.yaxis.set_major_formatter(EngFormatter(unit='A'))
+ax.yaxis.set_major_formatter(EngFormatter(unit='V'))
 ax.set_xlabel('Tiempo')
-ax.set_ylabel('Corriente')
+ax.set_ylabel('Tensión')
 #enable grid
 ax.grid(True)
 #ax.set_title(sys.argv[3])
@@ -40,9 +40,9 @@ ax.grid(True)
 if len(sys.argv) == 4:
     ax.set_xlim(float(sys.argv[2])-260e-9, float(sys.argv[3])-260e-9)
 # Divide y values by 10.8
-y = [i * 0.2 for i in y]
+#y = [i * 0.2 for i in y]
 # Inverti y values
-#y = [-i for i in y]
+y = [-i for i in y]
 ax.plot(x,y,color='red', linewidth=0.7)
 plt.show()
 #plt.savefig(sys.argv[2])
