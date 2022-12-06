6150f487123cfe8d98de3857cd71bf20a0ae1397
Enzo Sanchez Croce <enzo.sanchez.croce@alu.ing.unlp.edu.ar>
2022-12-06 11:41:27 -0300

    Co-authored-by: nicoelcapo98 <nicoelcapo98@users.noreply.github.com>

--- scripts/csv_to_image.py
+++ scripts/csv_to_image.py
@@ -31,9 +31,9 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 fig,ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 4))
 ax.xaxis.set_major_formatter(EngFormatter(unit='s'))
-ax.yaxis.set_major_formatter(EngFormatter(unit='V'))
+ax.yaxis.set_major_formatter(EngFormatter(unit='A'))
 ax.set_xlabel('Tiempo')
-ax.set_ylabel('Tensión')
+ax.set_ylabel('Corriente')
 #enable grid
 ax.grid(True)
 #ax.set_title(sys.argv[3])
@@ -41,9 +41,9 @@ ax.grid(True)
 if len(sys.argv) == 4:
     ax.set_xlim(float(sys.argv[2])-260e-9, float(sys.argv[3])-260e-9)
 # Divide y values by 10.8
-#y = [i * 0.2 for i in y]
+y = [i * 0.0001 for i in y]
 # Inverti y values
-y = [-i for i in y]
+#y = [-i for i in y]
 ax.plot(x,y,color='red', linewidth=0.7)
 cursor = Cursor(ax, useblit=True, color='red', linewidth=2)
 plt.show()
